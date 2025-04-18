b75ced58eeb8636f0c2d88709a16b50b3e70ba1d
Asif-ECE <md.asif.mostafa@outlook.com>
2025-04-19 01:42:44 +0600

    feat: updated test environment settings.

--- config/django/test.py
+++ config/django/test.py
@@ -1,1 +1,11 @@
 from .base import *
+
+EBUG = env.bool('DJANGO_DEBUG', default=True)
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=["*"])
+
+INSTALLED_APPS += [
+    'silk',
+]
+
+MIDDLEWARE = ['silk.middleware.SilkyMiddleware'] + MIDDLEWARE
